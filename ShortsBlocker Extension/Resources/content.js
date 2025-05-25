5089f89b1ddda299fff0fb5ccca6df3056c86660
Beyluta <beyluta1@gmail.com>
2025-05-25 16:42:18 +0200

    perf: improve removing bottom shorts icon

--- ShortsBlocker Extension/Resources/content.js
+++ ShortsBlocker Extension/Resources/content.js
@@ -1,23 +1,36 @@
 // Affected DOM elements
 const shortElements = [
-  'ytm-rich-section-renderer',  // Removes the shorts title
-  'ytd-reel-shelf-renderer',    // Removes the shorts themselves
-  '[class*=short]'              // Removes the bottom shorts icon
+  '.pivot-shorts',              // Make sure this is always the 0th element
+  'ytm-rich-section-renderer',  // Header of the shorts
+  'ytd-reel-shelf-renderer',    // Block containing a group of shorts
+  '[class*=short]'              // Anything else referencing shorts
 ];
                        
 // Purges all shorts from any source from the YouTube page
 function purgeShorts() {
-    // 1. Selecting all elements that reference shorts
+    // First we remove any icons from the bottom navigation bar
+    const elements = document.querySelectorAll(shortElements[0]);
+    for (const element of elements) {
+        element.parentElement.remove();
+    }
+    
+    // Then we remove all the other remaining elements
     const shorts = document.querySelectorAll(shortElements.join(', '));
-    // 2. Removing stored elements
-    for (const short of shorts) short.remove();
+    for (const short of shorts) {
+        short.remove();
+    }
 }
 
-// Initial purging of the shorts. Might not work
-// but leave this here just for peace of mind.
+/*
+** Initial purging of the shorts. Might not be needed
+** but leave this here just for peace of mind.
+*/
 purgeShorts();
 
-// Observing for any changes in the current page
+/*
+** Observing for any changes in the current page.
+** Used for whenever the user navigates to another page.
+*/
 const observer = new MutationObserver(purgeShorts);
 observer.observe(document.body, {
     childList: true,
